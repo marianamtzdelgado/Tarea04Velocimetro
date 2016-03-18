9cfba368f8a78ce2c6582212bd6831bfd2bd8090
Mariana Martínez <marianamtzdelgado@macbook-air-de-mariana.local>
2016-03-17 21:45:47 -0600

    Initial Commit

---
+++ Tarea04Velocimetro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tarea04Velocimetro
+//
+//  Created by Mariana Martínez Delgado on 17/03/16.
+//  Copyright © 2016 MMD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
